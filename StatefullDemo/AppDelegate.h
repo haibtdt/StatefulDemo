de9c382dc89e7507945442690d424d95610ceb1e
Bui Hai <haibtdt@gmail.com>
2015-06-29 23:28:34 +0700

    Initial Commit

---
+++ StatefullDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StatefullDemo
+//
+//  Created by Bui Hai on 6/29/15.
+//  Copyright (c) 2015 Bui Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StatefullDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StatefullDemo
+//
+//  Created by Bui Hai on 6/29/15.
+//  Copyright (c) 2015 Bui Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
